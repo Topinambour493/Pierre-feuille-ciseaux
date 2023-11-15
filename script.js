b80fa426bfe41f23746ce2e3a552b7f2241b11ff
cjezierski <cjezierski@softia.fr>
2023-11-15 17:23:39 +0100

    add diagram piechart

--- script.js
+++ script.js
@@ -3,8 +3,7 @@ const context = canvas.getContext('2d');
 context.imageSmoothingEnabled = false;
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
-imgs = []
-nb_elements=30
+
 
 function createImage(src, pos_actual, vector, radius){
     let newImage = new Image();
@@ -43,9 +42,7 @@ function collision(object1, object2){
     let dx = center_object1.x - center_object2.x;
     let dy = center_object1.y - center_object2.y;
     let distance = Math.sqrt(dx * dx + dy * dy);
-    if (distance < object1.radius){
-        return true
-    } return false
+    return distance < object1.radius
 }
 
 
@@ -72,51 +69,97 @@ function generator(nb_elements){
                 y: vectorPossibility[getRandomInt(2)]
             }
             img = createImage(src, pos_actual, vector, radius)
-        } while (manage_collision(img) > 0);
+        } while (manage_collision(img, true));
+        addNumberSrc(img)
         imgs.push(img);
     }
 }
 
+function getType(object){
+    return object.image.src.split("/")[object.image.src.split("/").length-1]
+}
+function addNumberSrc(object){
+    numbers_src[getType(object)] += 1
+}
+
+function withdrawNumberSrc(object){
+    numbers_src[getType(object)] -= 1
+}
+
+function change_src_image(object, url){
+    withdrawNumberSrc(object)
+    object.image.src = url
+    addNumberSrc(object)
+    updateDigramPiechart()
+}
+
+function updateDigramPiechart(){
+    document.getElementById('piechart').style.backgroundImage = 'conic-gradient('
+        + '#DBDBDB ' + degres("ciseaux.png").toString() + 'deg' + ', ' + "#487D1A 0 " + (degres("ciseaux.png")+degres("feuille.png")).toString() + 'deg, ' + "#D9C1AA 0" + ')';
+}
+
 function change_src_images(object1, object2){
-    src_object1 = object1.image.src.split("/")[object1.image.src.split("/").length-1]
-    src_object2 = object2.image.src.split("/")[object2.image.src.split("/").length-1]
+    src_object1 = getType(object1)
+    src_object2 = getType(object2)
     if (src_object1 == "feuille.png"){
         if (src_object2 == "pierre.png")
-            object2.image.src = "/images/feuille.png"
+            change_src_image(object2, "/images/feuille.png")
         else if (src_object2 == "ciseaux.png")
-            object1.image.src = "/images/ciseaux.png"
+            change_src_image(object1, "/images/ciseaux.png")
     } else if (src_object1 == "pierre.png"){
         if (src_object2 == "feuille.png")
-            object1.image.src = "/images/feuille.png"
+            change_src_image(object1, "/images/feuille.png")
         else if (src_object2 == "ciseaux.png")
-            object2.image.src = "/images/pierre.png"
+            change_src_image(object2, "/images/pierre.png")
     } else if (src_object1 == "ciseaux.png"){
         if (src_object2 == "pierre.png")
-            object1.image.src = "/images/pierre.png"
+            change_src_image(object1, "/images/pierre.png")
         else if (src_object2 == "feuille.png")
-            object2.image.src = "/images/ciseaux.png"
+            change_src_image(object2, "/images/ciseaux.png")
     }
 }
-function manage_collision(index_object) {
-    nb_collisions = 0;
+function manage_collision(index_object, call_by_generator) {
     object = imgs[index_object]
     for (let i = index_object+1; i < imgs.length ; i++){
         if (collision(object, imgs[i]) && object){
+            if (call_by_generator){
+                return true
+            }
             [object.vector, imgs[i].vector] = [imgs[i].vector, object.vector]
             change_src_images(object, imgs[i])
-            nb_collisions += 1
+
         }
     }
-    return nb_collisions
+    return false
 }
 
+function degres(type){
+    return numbers_src[type]*360/nb_elements
 
+}
+function restart(){
+    numbers_src = {"pierre.png": 0, "ciseaux.png": 0, "feuille.png": 0 }
+    nb_elements = document.getElementById("nb_elements").value
+    imgs = []
+    generator(nb_elements)
+    updateDigramPiechart()
+    document.getElementById("nb_elements").value = nb_elements
+}
+
+imgs = []
+nb_elements=document.getElementById("nb_elements").value
+numbers_src = {"pierre.png": 0, "ciseaux.png": 0, "feuille.png": 0 }
 generator(nb_elements)
+updateDigramPiechart
 setInterval(function(){
     context.clearRect(0, 0, context.canvas.width, context.canvas.height);
     for (let i = 0; i < imgs.length ; i++){
         manage_collision(i);
         move(imgs[i])
+    } if (numbers_src["pierre.png"] == nb_elements || numbers_src["ciseaux.png"] == nb_elements ||  numbers_src["feuille.png"] == nb_elements ){
+        setTimeout( restart, 200);
+
     }
 }, 1)
 
+document.getElementById("restart").addEventListener("click", restart);
