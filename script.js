981a3bf53bb3c8bb2af951c2a6d0fe59ffd1faaf
cjezierski <cjezierski@softia.fr>
2023-11-16 12:06:39 +0100

    gestion hit-box, responsive and gestion colllisions of settings

--- script.js
+++ script.js
@@ -45,18 +45,34 @@ function collision(object1, object2){
     return distance < object1.radius
 }
 
+function collisionSettingsRect(object){
+    settingsRect = document.getElementById("settings")
+    if (object.pos_actual.x < settingsRect.offsetWidth  &&  object.pos_actual.y < settingsRect.offsetHeight){
+        console.log(object.pos_actual.x, object.pos_actual.y,  settingsRect.offsetWidth, settingsRect.offsetHeight)
+        return true
+    }
+}
+
 
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
 }
 
+function collisionNewElement(object){
+    for (let i = 0; i < imgs.length ; i++) {
+        if (collision(object, imgs[i]) || collisionSettingsRect(object)) {
+            return true
+        }
+    }
+    return false
+}
 
 function generator(nb_elements){
     imagesSrc = ["/images/pierre.png", "/images/feuille.png", "/images/ciseaux.png"]
     vectorPossibility = [-1, 1]
     for (let i = 0; i < nb_elements; i++) {
         src = imagesSrc[getRandomInt(3)]
-        radius = 80;
+        radius = Math.min(window.innerWidth,window.innerHeight)/15;
 
         do {
             pos_actual = {
@@ -69,9 +85,9 @@ function generator(nb_elements){
                 y: vectorPossibility[getRandomInt(2)]
             }
             img = createImage(src, pos_actual, vector, radius)
-        } while (manage_collision(img, true));
+        } while (collisionNewElement(img));
+        imgs.push(img)
         addNumberSrc(img)
-        imgs.push(img);
     }
 }
 
@@ -118,16 +134,19 @@ function change_src_images(object1, object2){
             change_src_image(object2, "/images/ciseaux.png")
     }
 }
-function manage_collision(index_object, call_by_generator) {
+
+function manage_collision(index_object) {
     object = imgs[index_object]
-    for (let i = index_object+1; i < imgs.length ; i++){
-        if (collision(object, imgs[i]) && object){
-            if (call_by_generator){
-                return true
-            }
+    for (let i = index_object+1; i < imgs.length ; i++) {
+        if (collision(object, imgs[i])) {
             [object.vector, imgs[i].vector] = [imgs[i].vector, object.vector]
             change_src_images(object, imgs[i])
-
+        }
+    } if (collisionSettingsRect(object)) {
+        if (object.pos_actual.x+1 == settingsRect.offsetWidth){
+            object.vector.x *= -1
+        } else {
+            object.vector.y *= -1
         }
     }
     return false
@@ -150,14 +169,14 @@ imgs = []
 nb_elements=document.getElementById("nb_elements").value
 numbers_src = {"pierre.png": 0, "ciseaux.png": 0, "feuille.png": 0 }
 generator(nb_elements)
-updateDigramPiechart
+updateDigramPiechart()
 setInterval(function(){
     context.clearRect(0, 0, context.canvas.width, context.canvas.height);
     for (let i = 0; i < imgs.length ; i++){
         manage_collision(i);
         move(imgs[i])
     } if (numbers_src["pierre.png"] == nb_elements || numbers_src["ciseaux.png"] == nb_elements ||  numbers_src["feuille.png"] == nb_elements ){
-        setTimeout( restart, 200);
+        setTimeout( restart, 250);
 
     }
 }, 1)
